8bf79bdfa1be332cdb251399497cf40bf0558783
makzs <erikmakiak@gmail.com>
2024-04-07 19:17:12 -0300

    exercicio 3

---
+++ Exercicios/Lista05-Funções/Exercicio03.php
@@ -0,0 +1,22 @@
+<?php
+// 3. Construa uma função que desenhe um retângulo usando os caracteres ‘+’ ,‘−’ e ‘| ‘. 
+// Esta função deve receber dois parâmetros, linhas e colunas, sendoadicionado como padrão o valor mínimo
+//  igual a 
+// 1.  +----------- +
+//     |            |
+//     + ---------- +
+
+function formulario($nome, $numeros)
+{
+    echo "<h2> $nome </h2>";
+    echo '<form action="" method="GET">';
+
+    for ($i = 0; $i < $numeros; $i++) {
+        echo "<label>Informe o numero de linhas: </label>";
+        echo '<input type="text" name="valor' . ($i + 1) . '" id="valor' . ($i + 1) . '">';
+        echo "<br>";
+    }
+
+    echo '<button type="submit">Enviar</button>';
+    echo "</form>";
+}
