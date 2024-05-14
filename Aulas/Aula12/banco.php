b6d57fba369aa6564a6e879209876a2a11899d1b
makzs <erikmakiak@gmail.com>
2024-05-14 10:29:02 -0300

    aula 12 banco de dados

---
+++ Aulas/Aula12/banco.php
@@ -0,0 +1,45 @@
+<pre>
+
+<?php
+
+$banco = new mysqli("localhost:3307", "root", "", "tercamanha");
+
+// echo print_r($banco);
+
+// echo "<br><br><br>";
+// echo "busca";
+// echo "<br>--------------------------------------------<br>";
+
+// $buscar = $banco->query("SELECT * FROM usuarios");
+// echo print_r($buscar);
+
+// echo "<br><br><br>";
+// echo "busca geral de tabelas";
+// echo "<br>--------------------------------------------<br>";
+
+// while ($obj = $buscar->Fetch_object()) {
+
+//     echo print_r($obj);
+// }
+
+// echo "<br><br><br>";
+// echo "busca especifica de tabelas";
+// echo "<br>--------------------------------------------<br>";
+// $buscargdc = $banco->query("SELECT codigo, usuario, nome, senha FROM usuarios WHERE usuario='gdc1000'");
+
+// $usu = $buscargdc->fetch_object();
+// echo print_r($usu);
+
+// echo "<br><br><br>";
+// echo "Utilizando variaveis do banco";
+// echo "<br>--------------------------------------------<br>";
+
+// echo "<br>C: " . $usu->codigo;
+// echo "<br>U: " . $usu->usuario;
+// echo "<br>N: " . $usu->nome;
+// echo "<br>S: " . $usu->senha;
+
+// 
+?>
+
+</pre>
